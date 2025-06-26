343008f6b6eccacf8cc784efb32312e84bc7760f
Fanshi13 <aquillafazanaufan@gmail.com>
2025-06-26 11:53:45 +0700

    melengkapo void preorder

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -93,5 +93,18 @@ public:
         inorder(ptr->rightchild);
     }
 
-    void preorder(Node *ptr) {
-    };
+    void preorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+
+        cout << ptr->info << " ";
+        preorder(ptr->leftchild);
+        preorder(ptr->rightchild);
+    }
+};
