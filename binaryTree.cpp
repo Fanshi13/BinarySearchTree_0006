9da3b47968e41495cedfbfaa6ac17561ba6ef06e
Fanshi13 <aquillafazanaufan@gmail.com>
2025-06-26 11:58:33 +0700

    membuat case

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -144,5 +144,30 @@ public:
             char ch;
             cin >> ch;
             cout << endl;
-        }
-    };
+
+            switch (ch)
+            {
+            case '1':
+            {
+                x.insert();
+                break;
+            }
+            case '2':
+            {
+                x.inorder(x.ROOT);
+                break;
+            }
+            case '3':
+            {
+
+                x.preorder(x.ROOT);
+                break;
+            }
+            case '4':
+            {
+
+                x.postorder(x.ROOT);
+                break;
+            }
+            }
+        };
