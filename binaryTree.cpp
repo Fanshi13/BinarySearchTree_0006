16b33eacb94873385202d47f2e7d33da66eb40ee
Fanshi13 <aquillafazanaufan@gmail.com>
2025-06-26 11:46:01 +0700

    membuat class binaryTree

---
+++ binaryTree.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    Node()
+    {
+        leftchild = nullptr;
+        rightchild = nullptr;
+    }
+};
+
+class BinaryTree
+{
+public:
+    Node *ROOT;
+
+    BinaryTree()
+    {
+        ROOT = nullptr;
+    }
+};
