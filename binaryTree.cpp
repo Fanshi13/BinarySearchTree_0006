5dd0e71ca1046596914bef191bc26eb949751c69
Fanshi13 <aquillafazanaufan@gmail.com>
2025-06-26 11:54:58 +0700

    membuat void preorder

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -107,4 +107,24 @@ public:
         preorder(ptr->leftchild);
         preorder(ptr->rightchild);
     }
+
+    void postorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+
+        postorder(ptr->leftchild);
+        postorder(ptr->rightchild);
+        cout << ptr->info << " ";
+    }
+
+    bool isEmpty()
+    {
+        return ROOT == nullptr;
+    }
 };
